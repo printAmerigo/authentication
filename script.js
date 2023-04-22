13db6378d2a6a7db1c1d591c528ce634b77c5e31
printAmerigo <128327716+printAmerigo@users.noreply.github.com>
2023-04-22 12:06:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function auth() {
+  var formEmail = document.getElementById("emailData").value;
+  var formPassword = document.getElementById("passwordData").value;
+  var localemail = localStorage.getItem("localemail");
+  var localpassword = localStorage.getItem("localpassword");
+  
+  if (formEmail == localemail && formPassword == localpassword) {
+    var email = formEmail
+    var password = formPassword.slice(0, -4) + '****';
+    alert(`Welcome to the site, ${email}. We see your password is ${password}`)
+  } else {
+    alert("Hmm... It seems this Info was not registered in this session. Please go to the registration page!")
+  }
+}
+document.getElementById("loginButton").addEventListener("click", auth);
+
+
+
